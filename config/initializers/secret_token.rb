fda1cd139eeae01d8e30a94afc861e235e0eb74c
railion <railion@suremail.info>
2011-08-28 22:46:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '35a87582f5a7392f0a427e8f34bd73875d57da86dadb5835d6d78cfaad0b8dd73b7a63557537bfc5d94cd29359f3d18c4775996f4dd1a76a2e4b56f1cd385ddb'
